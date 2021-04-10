f3863ced328dd113d9fd128b127ea3d9f7cc81ff
Darian Vereen <vereen.darian@gmail.com>
2021-04-09 21:25:56 -0400

    Add base url to php file(s). Update URI paths of all links.

--- spaceInvaders.php
+++ spaceInvaders.php
@@ -1,5 +1,6 @@
 <?php include_once('headNoNav.php');?> 
         <title>DJV Space Invaders</title>
+        <base href = "/portfolio/spaceInvaders/">
         <link href = "/CSS/allProjectsModal.css" rel = "stylesheet">
         <link rel="stylesheet" href="spaceInvaders.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
